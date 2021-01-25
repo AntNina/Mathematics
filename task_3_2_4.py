a7e08fa41b3cc60ac0b6ce3c3508cc63433c842f
Nina Antanishyna <jockobooboo@gmail.com>
2021-01-25 13:21:13 +0300

    homework_3

---
+++ task_3_2_4.py
@@ -0,0 +1,30 @@
+# %matplotlib inline
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+from mpl_toolkits.mplot3d import Axes3D
+import warnings
+
+warnings.filterwarnings('ignore')
+
+x = []
+x2 = []
+y = []
+y2 = []
+
+for i in range(1000):
+    a = 400
+    b = 200
+    x.append(-i)
+    x2.append(i)
+    y.append(np.sqrt(b ** 2 + (b ** 2 * (i ** 2 / a ** 2))))
+    y2.append(-np.sqrt(b ** 2 + (b ** 2 * (i ** 2 / a ** 2))))
+
+plt.plot(x, y, color='r')
+plt.plot(x, y2, color='r')
+plt.plot(x2, y2, color='r')
+plt.plot(x2, y, color='r')
+plt.axis('scaled')
+plt.show()
